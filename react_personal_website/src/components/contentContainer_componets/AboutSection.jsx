cd780ca729b3367d7c9fa94863457ccb488d28d2
henok206 <henok206@uw.edu>
2025-09-05 20:52:07 -0700

    Adjust about me text.

--- react_personal_website/src/components/contentContainer_componets/AboutSection.jsx
+++ react_personal_website/src/components/contentContainer_componets/AboutSection.jsx
@@ -16,6 +16,9 @@ function AboutSection() {
         <p className="card-text">
           I'm very passionate about my teaching work. My experiences as a TA have definitely been the highlight of my time in college. In my free time I love to watch anime, play badminton, go on runs, and try out new restaurants in the greater Seattle area.
         </p>
+        <p className="card-text">
+          If you have any questions, feel free to shoot me an email or reach out on any platforms that I have shared. I'll get back to you as soon as I can!
+        </p>
       </div>
     </>
   )
